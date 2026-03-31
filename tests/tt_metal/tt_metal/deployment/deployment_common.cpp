b3a1118d1e82dc3b26014c1dff6cbbd4d023418c
Никола Радојевић <nikola.radojevic@outlogix.com>
2026-03-31 16:31:31 +0200

    Clean up the signal handling

--- tests/tt_metal/tt_metal/deployment/deployment_common.cpp
+++ tests/tt_metal/tt_metal/deployment/deployment_common.cpp
@@ -1,12 +1,9 @@
 #include "deployment_common.hpp"
 
 std::atomic_bool g_stop_requested = false;
-std::atomic_bool g_stop_message_printed = false;
 
 void handle_sigint(int) {
-    g_stop_requested.store(true);
-
-    if (!g_stop_message_printed.exchange(true)) {
+    if (!g_stop_requested.exchange(true)) {
         const char msg[] = "\nSIGINT received, waiting to finish current test...\n";
         write(2, msg, sizeof msg - 1);
     }
